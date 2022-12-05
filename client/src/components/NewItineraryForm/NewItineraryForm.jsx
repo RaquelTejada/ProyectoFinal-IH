13322035121888c56b7aa5c957260bdfd71de97f
Raquel Tejada de Jesus <tejadadejesusraquel@gmaile.com>
2022-12-05 18:23:21 +0100

    new itinerary form

---
+++ client/src/components/NewItineraryForm/NewItineraryForm.jsx
@@ -0,0 +1,86 @@
+import { useState } from "react"
+import { Form, Button, Row, Col } from "react-bootstrap"
+import itinerariesService from "../../services/itineraries.service"
+
+const NewItineraryForm = ({ fireFinalActions }) => {
+
+    const [itineraryData, setItineraryData] = useState({
+        city: '',
+        transport: '',
+        category: '',
+        title: '',
+        duration: '',
+        pets: false,
+        description: '',
+        images: ''
+    })
+
+    const handleInputChange = e => {
+        const { name, value } = e.target
+        itineraryData({ ...itineraryData, [name]: value })
+    }
+
+    const handleFormSubmit = e => {
+        e.preventDefault()
+
+        itinerariesService
+            .saveItinerary(itineraryData)
+            .then(() => {
+                fireFinalActions()
+            })
+            .catch(err => console.error(err))
+    }
+
+    const { city, transport, category, title, duration, pets, description, images } = itineraryData
+
+
+    return (
+        <Form onSubmit={handleFormSubmit}>
+            <Form.Group className="mb-3" controlId="name">
+                <Form.Label>Ciudad</Form.Label>
+                <Form.Control type="text" value={city} onChange={handleInputChange} name="city" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="desc">
+                <Form.Label>Medio de transporte</Form.Label>
+                <Form.Control type="text" value={transport} onChange={handleInputChange} name="transport" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="desc">
+                <Form.Label>Categoría</Form.Label>
+                <Form.Control type="text" value={category} onChange={handleInputChange} name="category" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="desc">
+                <Form.Label>Nombre de la ruta</Form.Label>
+                <Form.Control type="text" value={title} onChange={handleInputChange} name="title" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="desc">
+                <Form.Label>Duración</Form.Label>
+                <Form.Control type="text" value={duration} onChange={handleInputChange} name="duration" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="desc">
+                <Form.Label>¿Se puede hacer con animales?</Form.Label>
+                <Form.Control type="radio" value={pets} onChange={handleInputChange} name="pets" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="desc">
+                <Form.Label>Descripción</Form.Label>
+                <Form.Control type="text area" value={description} onChange={handleInputChange} name="description" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="desc">
+                <Form.Label>Imágenes</Form.Label>
+                <Form.Control type="file" multiple value={images} onChange={handleInputChange} name="images" />
+            </Form.Group>
+
+            <div className="d-grid">
+                <Button variant="dark" type="submit">Crear ruta</Button>
+            </div>
+        </Form>
+    )
+}
+
+export default NewItineraryForm
