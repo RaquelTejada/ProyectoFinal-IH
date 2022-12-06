d9e5cdd1ce15d32f4b49cd8f2b3ba9333e5fe186
Raquel Tejada de Jesus <tejadadejesusraquel@gmaile.com>
2022-12-06 15:33:21 +0100

    itinerarylist, itinerary card, home page

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,21 +1,84 @@
 import { Container, Row, Col, Button } from 'react-bootstrap'
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './HomePage.css'
+import { Card } from 'react-bootstrap'
+import Madrid from '../../images/Madrid-ABC.jpg'
+import Roma from '../../images/Roma.jpg'
 
 const HomePage = () => {
     return (
-        <Container className="Home">
-            <Row>
-                <Col md={{ span: 8, offset: 2 }}>
-                    <h1>Itineraies App!</h1>
-                    <hr />
-                    <h3>Proyecto final Ironhack</h3>
-                    {/* <Link to="/galeria">
-                        <Button variant="dark">Ir a la galería</Button>
-                    </Link> */}
-                </Col>
-            </Row>
-        </Container>
+        <div className='main-div-home-page'>
+            <Container fluid className="margin-card">
+                <Row>
+                    <Col >
+                        <Card className="bg-gray card-size">
+                            <Card.Img src={Madrid} alt="Card image" />
+                            <Card.ImgOverlay>
+                                <Card.Title className='card-title'>MADRID</Card.Title>
+                            </Card.ImgOverlay>
+                        </Card>
+                    </Col>
+                    <Col >
+                        <Card className="bg-gray card-size">
+                            <Card.Img src={Roma} alt="Card image" />
+                            <Card.ImgOverlay>
+                                <Card.Title className='card-title'>MADRID</Card.Title>
+                            </Card.ImgOverlay>
+                        </Card>
+                    </Col>
+                    <Col >
+                        <Card className="bg-gray card-size">
+                            <Card.Img src={Madrid} alt="Card image" />
+                            <Card.ImgOverlay>
+                                <Card.Title className='card-title'>MADRID</Card.Title>
+                            </Card.ImgOverlay>
+                        </Card>
+                    </Col>
+                    <Row>
+                        <Container fluid className="margin-card">
+
+                        </Container>
+                    </Row>
+                    <Col >
+                        <Card className="bg-gray card-size">
+                            <Card.Img src={Madrid} alt="Card image" />
+                            <Card.ImgOverlay>
+                                <Card.Title className='card-title'>MADRID</Card.Title>
+                            </Card.ImgOverlay>
+                        </Card>
+                    </Col>
+                    <Col >
+                        <Card className="bg-gray card-size">
+                            <Card.Img src={Madrid} alt="Card image" />
+                            <Card.ImgOverlay>
+                                <Card.Title className='card-title'>MADRID</Card.Title>
+                            </Card.ImgOverlay>
+                        </Card>
+                    </Col>
+                    <Col >
+                        <Card className="bg-gray card-size">
+                            <Card.Img src={Madrid} alt="Card image" />
+                            <Card.ImgOverlay>
+                                <Card.Title className='card-title'>MADRID</Card.Title>
+                            </Card.ImgOverlay>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     )
 }
 export default HomePage
+
+            // <Container className="Home">
+            //     <Row>
+            //         <Col md={{ span: 8, offset: 2 }}>
+            //             <h1>Itineraies App!</h1>
+            //             <hr />
+            //             <h3>Proyecto final Ironhack</h3>
+            //             {/* <Link to="/galeria">
+            //             <Button variant="dark">Ir a la galería</Button>
+            //         </Link> */}
+            //         </Col>
+            //     </Row>
+            // </Container>
